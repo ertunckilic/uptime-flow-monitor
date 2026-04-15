a7bc723607c4a153e4a6ce1e09e7400674386459
dec4bria <dec4bria@gmail.com>
2026-04-16 00:32:33 +0300

    Last updates settled.

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -27,10 +27,10 @@ export async function GET(request: Request) {
   // Resend motorunu sadece cron çalıştığında uyanacak şekilde buraya aldık
   const resend = new Resend(process.env.RESEND_API_KEY!);
 
-  const authHeader = request.headers.get('authorization');
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  }
+  // const authHeader = request.headers.get('authorization');
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //  return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  // }
 
   const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
   
